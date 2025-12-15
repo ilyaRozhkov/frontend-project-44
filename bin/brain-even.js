05d313db4089ac1fa34e092b7d48422f19668b01
ilyaRozhkov <56915075+ilyaRozhkov@users.noreply.github.com>
2025-12-15 20:53:57 +0300

    Update brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
 import {name} from '../src/cli.js'
 import { isEven } from '../src/games/even.js'
 import { indexDriver } from '../src/index.js'
 
 console.log(`Hello, ${name}!`)
 console.log('Answer "yes" if the number is even, otherwise answer "no".')
+
 indexDriver(isEven)
